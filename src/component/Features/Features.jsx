a2e71102b7729fff41cdfc83e35883add94160a4
Abu-Sahad <abusahad603@gmail.com>
2023-05-11 16:22:30 +0600

    set see more button and implement button work

--- src/component/Features/Features.jsx
+++ src/component/Features/Features.jsx
@@ -1,31 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Feature from '../Feature/Feature';
 
 const Features = () => {
-    const [jobs, setJobs] = useState([])
+    const [jobs, setJobs] = useState([]);
+    const [visibleJobs, setVisibleJobs] = useState(4); // initially display 4 items
     useEffect(() => {
         fetch('jobhut.json')
             .then(res => res.json())
             .then(data => setJobs(data))
-    }, [])
+    }, []);
+
+    const handleSeeMore = () => {
+        setVisibleJobs(prevCount => prevCount + 4); // display 4 more items on button click
+    };
+
     return (
         <>
             <div className='ms-32 mb-10 grid grid-cols-1 md:grid-cols-2 gap-20 me-32'>
                 {
-                    jobs.map(job => <Feature
-                        key={job.id}
-                        job={job}
-
-                    ></Feature>)
+                    jobs.slice(0, visibleJobs).map(job => (
+                        <Feature
+                            key={job.id}
+                            job={job}
+                        ></Feature>
+                    ))
                 }
             </div>
 
-            <div className='text-center mb-10'>
-                <Link to="details" className=" btn-Common inline-block px-6 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
-                    See More
-                </Link>
-            </div>
+            {visibleJobs < jobs.length && (
+                <div className='text-center mb-10'>
+                    <button onClick={handleSeeMore} className="btn-Common inline-block px-6 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
+                        See More
+                    </button>
+                </div>
+            )}
         </>
     );
 };
