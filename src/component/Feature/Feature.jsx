eac69a276c432e4319dda59254df13988e757d20
Abu-Sahad <abusahad603@gmail.com>
2023-05-11 16:17:29 +0600

    feature job half added

--- src/component/Feature/Feature.jsx
+++ src/component/Feature/Feature.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
-const Feature = () => {
+import { Link } from 'react-router-dom';
+import './Feature.css'
+const Feature = ({ job }) => {
+    const { logo, job_title, company_name, location, salary, } = job
+    // console.log(job)
     return (
-        <div>
-
+        <div className='future-main-part-area'>
+            <img className="w-70 h-70 pb-4" src={logo} alt="Logo" />
+            <h2 className="text-xl pb-2 font-bold">{job_title}</h2>
+            <p className="text-gray-600 mb-4">{company_name}</p>
+            <div>
+                <button className='future-part-button'>{job.job_type_details.job_type1}</button>
+                <button className='future-part-button'>{job.job_type_details.job_type2}</button>
+            </div>
+            <div className='flex mt-4 mb-4'>
+                <p className='me-4'>{location}</p>
+                <p>{salary}</p>
+            </div>
+            <Link to="details" className="btn-Common inline-block px-6 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
+                View Details
+            </Link>
         </div>
     );
 };
--- src/component/Features/Features.jsx
+++ src/component/Features/Features.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Feature from '../Feature/Feature';
 
 const Features = () => {
+    const [jobs, setJobs] = useState([])
+    useEffect(() => {
+        fetch('jobhut.json')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
     return (
-        <div>
+        <>
+            <div className='ms-32 mb-10 grid grid-cols-1 md:grid-cols-2 gap-20 me-32'>
+                {
+                    jobs.map(job => <Feature
+                        key={job.id}
+                        job={job}
 
-        </div>
+                    ></Feature>)
+                }
+            </div>
+
+            <div className='text-center mb-10'>
+                <Link to="details" className=" btn-Common inline-block px-6 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
+                    See More
+                </Link>
+            </div>
+        </>
     );
 };
 
