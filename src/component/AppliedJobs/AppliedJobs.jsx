3a2fdd6a8a04c40211b0756a802fc89364b64a6c
Abu-Sahad <abusahad603@gmail.com>
2023-05-12 00:19:20 +0600

    added applied job area

--- src/component/AppliedJobs/AppliedJobs.jsx
+++ src/component/AppliedJobs/AppliedJobs.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
 import { getShoppingCart } from '../../utilities/fakedb';
-
+import { MapPinIcon, CurrencyDollarIcon, BarsArrowDownIcon } from '@heroicons/react/24/solid'
+import './AppliedJobs.css'
 
 const AppliedJobs = () => {
     const shoppingCart = getShoppingCart();
     const cartItems = Object.entries(shoppingCart);
     return (
         <div>
-            <h1>Applied Jobs:</h1>
-            {cartItems.map(([id, { job, quantity }]) => (
+            <div>
+                <h1 className='applied-job-details-tittle md:bg-[#E5E7EB]'>Applied Job</h1>
+            </div>
+            <button className='applied-button-component'>
+                Filter By
+                <BarsArrowDownIcon className="h-6 w-6 text-blue-500 ms-2"/>
+            </button>
+
+            {cartItems.map(([id, { job }]) => (
                 <div key={id}>
-                    <h2>{job?.job_title}</h2>
-                    <p>{job?.job_description}</p>
-                    <p>Quantity: {quantity}</p>
+                    <div className='applied-component-area'>
+                        {/* <h2>{job?.job_title}</h2>
+                        <p>{job?.job_description}</p>
+                        <p>Quantity: {quantity}</p> */}
+
+                        <div className='applied-job-logo'>
+                            <img className="w-70 h-70 pb-4" src={job?.logo} alt="Logo" /><img src="" alt="" />
+                        </div>
+                        <div>
+                            <h2>{job?.job_title}</h2>
+                            <h2>{job?.company_name}</h2>
+                            <div className='pt-4'>
+                                <button className='future-part-button'>{job.job_type_details.job_type1}</button>
+                                <button className='future-part-button'>{job.job_type_details.job_type2}</button>
+                            </div>
+                            <div className='flex mt-4 mb-4'>
+                                <MapPinIcon className="h-6 w-6 text-blue-500" />
+                                <p className='me-4'>{job?.location}</p>
+                                <CurrencyDollarIcon className="h-6 w-6 text-blue-500" />
+                                <p>{job?.salary}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn-Common inline-block px-6 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
+                                View Details
+                            </button>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
